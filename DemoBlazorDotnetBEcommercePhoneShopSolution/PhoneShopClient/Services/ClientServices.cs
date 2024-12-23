01839092293ca3be69d2b983c40ba81f4d284d4e
Ryan Hoang <anhoang800140@gmail.com>
2024-12-23 17:06:24 +0700

    Adding categories service and optimize API from client

--- DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopClient/Services/ClientServices.cs
+++ DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopClient/Services/ClientServices.cs
@@ -1,44 +1,113 @@
 ﻿using PhoneShopShareLibrary.Models;
 using PhoneShopShareLibrary.Responses;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace PhoneShopClient.Services
 {
-    public class ClientServices(HttpClient httpClient) : IProductService
+    public class ClientServices(HttpClient httpClient) : IProductService, ICategoryService
     {
-        private const string BaseUrl = "api/product";
-        private static string SerializedObj(object modelObj) => JsonSerializer.Serialize(modelObj, JsonOptions());
-        private static T DeserializeJsonString<T>(string jsonString) => JsonSerializer.Deserialize<T>(jsonString, JsonOptions())!;
-        private static StringContent GenerateStringContent(string serializedObj) => new(serializedObj, System.Text.Encoding.UTF8, "application/json");
-        private static IList<T> DeserializeJsonStringList<T>(string jsonString) => JsonSerializer.Deserialize<IList<T>>(jsonString, JsonOptions())!;
-        private static JsonSerializerOptions JsonOptions()
-        {   
-            return new JsonSerializerOptions
-            {
-                AllowTrailingCommas = true,
-                PropertyNameCaseInsensitive = true,
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip
-            };
-        }
+        private const string ProductBaseUrl = "api/product";
+        private const string CategoryBaseUrl = "api/category";
+
+        public Action? CategoryAction { get; set; }
+        public List<Category> AllCategories { get; set; }
+        public Action? ProductAction { get; set; }
+        public List<Product> AllProducts { get; set; }
+        public List<Product> FeaturedProducts { get; set; }
+
+
+        //product
 
         public async Task<ServiceResponse> AddProductAsync(Product product)
         {
-            var response = await httpClient.PostAsync(BaseUrl, GenerateStringContent(SerializedObj(product)));
-            if(!response.IsSuccessStatusCode)
-                return new ServiceResponse(false, "Error occured. Try again later...");
-            var apiResponse = await response.Content.ReadAsStringAsync();
-            return DeserializeJsonString<ServiceResponse>(apiResponse);
+            var response = await httpClient.PostAsync(ProductBaseUrl, General.GenerateStringContent(General.SerializedObj(product)));
+
+            var result = CheckResponse(response);
+            if (!result.flag) return result;
+
+            var apiResponse = await ReadContent(response);
+            await ClearAndGetAllProduct();
+            return General.DeserializeJsonString<ServiceResponse>(apiResponse);
         }
 
-        public async Task<List<Product>> GetAllProductsAsync(bool featuredProducts)
+        private async Task ClearAndGetAllProduct()
         {
-            var response = await httpClient.GetAsync($"{BaseUrl}?featured={featuredProducts}");
+            bool featuredProducts = true;
+            bool allProducts = false;
+            AllProducts = null!;
+            FeaturedProducts = null!;
+            await GetAllProductsAsync(featuredProducts);
+            await GetAllProductsAsync(allProducts);
+        }
+
+        public async Task GetAllProductsAsync(bool featuredProducts)
+        {
+            if (featuredProducts && FeaturedProducts is null)
+            {
+                FeaturedProducts = await GetProducts(featuredProducts);
+                ProductAction?.Invoke();
+                return;
+            }
+            else
+            {
+                if (!featuredProducts && AllProducts is null)
+                {
+                    AllProducts = await GetProducts(featuredProducts);
+                    ProductAction?.Invoke();
+                    return;
+                }
+            }
+
+
+        }
+        private async Task<List<Product>> GetProducts(bool featured)
+        {
+            var response = await httpClient.GetAsync($"{ProductBaseUrl}?featured={featured}");
+            var (flag, _) = CheckResponse(response);
+            if (!flag) return null!;
+            var result = await ReadContent(response);
+            return (List<Product>?)General.DeserializeJsonStringList<Product>(result)!;
+        }
+
+        //category
+
+        public async Task<ServiceResponse> AddCategoryAsync(Category category)
+        {
+            var response = await httpClient.PostAsync(CategoryBaseUrl, General.GenerateStringContent(General.SerializedObj(category)));
+
+            var result = CheckResponse(response);
+            if (!result.flag) return result;
+
+            var apiResponse = await ReadContent(response);
+            await ClearAndGetAllCategory();
+            return General.DeserializeJsonString<ServiceResponse>(apiResponse);
+        }
+
+        public async Task GetAllCategoriesAsync()
+        {
+            if (AllCategories != null)
+            {
+                var response = await httpClient.GetAsync($"{CategoryBaseUrl}");
+                var (flag, _) = CheckResponse(response);
+                if (!flag) return;
+                var result = await ReadContent(response);
+                AllCategories = (List<Category>?)General.DeserializeJsonStringList<Category>(result)!;
+                CategoryAction?.Invoke();
+            }
+        }
+
+        private async Task ClearAndGetAllCategory()
+        {
+            AllCategories = null!;
+            await GetAllCategoriesAsync();
+        }
+        //General method
+        private async Task<String> ReadContent(HttpResponseMessage response) => await response.Content.ReadAsStringAsync();
+
+        private ServiceResponse CheckResponse(HttpResponseMessage response)
+        {
             if (!response.IsSuccessStatusCode)
-                return null!;
-            var result = await response.Content.ReadAsStringAsync();
-            return [.. DeserializeJsonStringList<Product>(result)];
+                return new ServiceResponse(false, "Error occured. Try again later...");
+            else return new ServiceResponse(true, null!);
         }
     }
 }
