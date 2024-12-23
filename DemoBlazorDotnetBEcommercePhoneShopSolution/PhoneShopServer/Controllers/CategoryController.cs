726aafc7f3d80360c3e43c52f1a9abe82a640f87
Ryan Hoang <anhoang800140@gmail.com>
2024-12-23 15:38:46 +0700

    Adding api of category

---
+++ DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopServer/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneShopServer.Repositories;
+using PhoneShopShareLibrary.Models;
+
+namespace PhoneShopServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategory _categoryService;
+        public CategoryController(ICategory categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            return Ok(categories);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Category category)
+        {
+            if (category is null) return BadRequest("Category is null");
+            var response = await _categoryService.AddCategoryAsync(category);
+            return Ok(response);
+        }
+    }
+}
