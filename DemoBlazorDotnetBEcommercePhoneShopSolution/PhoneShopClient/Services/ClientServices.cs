2e98d71302610f10b91761de46ccd959afcbcb6d
Ryan Hoang <anhoang800140@gmail.com>
2024-12-30 22:04:14 +0700

    Fix error can't load category

--- DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopClient/Services/ClientServices.cs
+++ DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopClient/Services/ClientServices.cs
@@ -10,15 +10,15 @@ namespace PhoneShopClient.Services
         private const string CategoryBaseUrl = "api/Category";
 
         public Action? CategoryAction { get; set; }
-        public List<Category> AllCategories { get; set; } = new();
+        public List<Category> AllCategories { get; set; }
         public Action? ProductAction { get; set; }
-        public List<Product> AllProducts { get; set; } = new();
-        public List<Product> FeaturedProducts { get; set; } = new();
+        public List<Product> AllProducts { get; set; }
+        public List<Product> FeaturedProducts { get; set; } 
 
 
         //product
 
-        public async Task<ServiceResponse> AddProductAsync(Product product)
+        public async Task<ServiceResponse> AddProduct(Product product)
         {
             var response = await httpClient.PostAsync(ProductBaseUrl, General.GenerateStringContent(General.SerializedObj(product)));
 
@@ -38,11 +38,11 @@ namespace PhoneShopClient.Services
             bool allProducts = false;
             AllProducts = null!;
             FeaturedProducts = null!;
-            await GetAllProductsAsync(featuredProducts);
-            await GetAllProductsAsync(allProducts);
+            await GetAllProducts(featuredProducts);
+            await GetAllProducts(allProducts);
         }
 
-        public async Task GetAllProductsAsync(bool featuredProducts)
+        public async Task GetAllProducts(bool featuredProducts)
         {
             if (featuredProducts && FeaturedProducts is null)
             {
@@ -73,7 +73,7 @@ namespace PhoneShopClient.Services
 
         //category
 
-        public async Task<ServiceResponse> AddCategoryAsync(Category category)
+        public async Task<ServiceResponse> AddCategory(Category category)
         {
             var response = await httpClient.PostAsync(CategoryBaseUrl, General.GenerateStringContent(General.SerializedObj(category)));
 
@@ -88,9 +88,9 @@ namespace PhoneShopClient.Services
             return data;
         }
 
-        public async Task GetAllCategoriesAsync()
+        public async Task GetAllCategories()
         {
-            if (AllCategories == null)
+            if (AllCategories is null)
             {
                 var response = await httpClient.GetAsync($"{CategoryBaseUrl}");
                 var (flag, _) = CheckResponse(response);
@@ -104,7 +104,7 @@ namespace PhoneShopClient.Services
         private async Task ClearAndGetAllCategory()
         {
             AllCategories = null!;
-            await GetAllCategoriesAsync();
+            await GetAllCategories();
         }
         //General method
         private async Task<string> ReadContent(HttpResponseMessage response) => await response.Content.ReadAsStringAsync();
--- DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopClient/Services/IProductService.cs
+++ DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopClient/Services/IProductService.cs
@@ -6,10 +6,9 @@ namespace PhoneShopClient.Services
     public interface IProductService
     {
         Action? ProductAction { get; set; }
-        Task<ServiceResponse> AddProductAsync(Product product);
-        Task GetAllProductsAsync(bool featuredProducts);
+        Task<ServiceResponse> AddProduct(Product product);
+        Task GetAllProducts(bool featuredProducts);
         List<Product> AllProducts { get; set; }
-
         List<Product> FeaturedProducts { get; set; }
     }
 }
