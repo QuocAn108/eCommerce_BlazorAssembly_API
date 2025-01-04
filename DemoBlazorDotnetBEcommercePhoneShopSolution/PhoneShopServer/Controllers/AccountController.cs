651b07b4b011826ac2710f2fe49a3c224180b7f8
Ryan Hoang <anhoang800140@gmail.com>
2025-01-04 23:22:24 +0700

    Add api processing for login and token of user

--- DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopServer/Controllers/AccountController.cs
+++ DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopServer/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ namespace PhoneShopServer.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserDTO model)
         {
-            if(model is null)
+            if (model is null)
                 return BadRequest("Model is null");
             var response = await userAccount.Register(model);
             return Ok(response);
@@ -24,6 +24,41 @@ namespace PhoneShopServer.Controllers
                 return BadRequest("Model is null");
             var response = await userAccount.Login(model);
             return Ok(response);
+        }
+
+        [HttpGet("user-info")]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            var token = GetTokenFromHeader();
+            if (token is null)
+                return Unauthorized("Token is null");
+            var getUser = await userAccount.GetUserByToken(token!);
+            if (getUser is null || string.IsNullOrEmpty(getUser.Email))
+                return Unauthorized("User not found");
+            return Ok(getUser);
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken(PostRefreshTokenDTO model)
+        {
+            if (model is null)
+                return Unauthorized();
+            var response = await userAccount.GetRefreshToken(model);
+            return Ok(response);
+        }
+
+        private string GetTokenFromHeader()
+        {
+            string Token = string.Empty;
+            foreach (var header in Request.Headers)
+            {
+                if (header.Key.ToString().Equals("Authorization"))
+                {
+                    Token = header.Value.ToString();
+                    break;
+                }
+            }
+            return Token.Split(" ").LastOrDefault()!;
         }
     }
 }
