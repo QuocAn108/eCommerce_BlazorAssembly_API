d427324a9c6c9b154b71859d5fb304ce2850ff59
Ryan Hoang <anhoang800140@gmail.com>
2025-01-04 10:18:31 +0700

    Create IUserAccount and another DTO for this

---
+++ DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopServer/Repositories/IUserAccount.cs
@@ -0,0 +1,14 @@
+﻿using PhoneShopShareLibrary.DTOs;
+using PhoneShopShareLibrary.Responses;
+
+namespace PhoneShopServer.Repositories
+{
+    public interface IUserAccount
+    {
+        Task<ServiceResponse> Register(UserDTO model);
+        Task<LoginResponse> Login(UserDTO model);
+        Task<UserSession> GetUserByToken(string token);
+        Task<LoginResponse> GetRefreshToken(PostRefreshTokenDTO model);
+
+    }
+}
--- DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopShareLibrary/Responses/ServiceResponse.cs
+++ DemoBlazorDotnetBEcommercePhoneShopSolution/PhoneShopShareLibrary/Responses/ServiceResponse.cs
@@ -2,4 +2,5 @@
 namespace PhoneShopShareLibrary.Responses
 {
     public record class ServiceResponse(bool flag, string message);
+    public record class LoginResponse(bool flag, string? message, string token = null!, string refreshToken = null!);
 }
